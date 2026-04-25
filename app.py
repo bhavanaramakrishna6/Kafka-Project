89f02d9ba68a81a4e69ca0f3e1722bacf7847f6e
ramakrishnabhavana <rbhavanacm2a@gmail.com>
2026-04-25 10:31:43 +0000

    fixed image path

--- app.py
+++ app.py
@@ -4,11 +4,29 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+# In-memory storage (for deployment version)
 counts = {}
+
+# ------------------ ROUTES ------------------
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
+
+# ------------------ CLICK TRACKING ------------------
 
 @app.route('/click', methods=['POST'])
 def click():
@@ -23,6 +41,9 @@ def click():
     print("Current Counts:", counts)
 
     return {"status": "success"}
+
+
+# ------------------ RUN APP ------------------
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
